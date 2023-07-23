61f3afbec731842f3bf98f2fab6e2152cc5e18bc
m5imunovic <simunovic.marijo@gmail.com>
2023-07-23 16:20:36 +0200

    Merge

--- src/utils/image_ops.py
+++ src/utils/image_ops.py
@@ -15,9 +15,36 @@ def rgb_to_grayscale(input_path: Path) -> np.ndarray:
     return grayscale_np
 
 
-def image_to_np_array(input_img: Path | Image.Image) -> np.ndarray:
+def image_to_np_array(input_img: Path | Image.Image | np.ndarray) -> np.ndarray:
+    if isinstance(input_img, np.ndarray):
+        return input_img
+
     if isinstance(input_img, Path):
         input_img = Image.open(input_img, "r")
 
     img_np = np.array(input_img)
     return img_np
+
+
+def mix_images(input_images: list[Path | Image.Image | np.ndarray], mixing_matrix: np.ndarray) -> list:
+
+    matrix_rows, matrix_cols = mixing_matrix.shape
+    assert matrix_cols == matrix_rows, "We expect the same number of sources and targets"
+
+    assert len(input_images) == matrix_cols, "Number of images does not match mixing matrix dimensions"
+
+    images = [image_to_np_array(image_path) for image_path in input_images]
+
+    # Flatten the images into 1D arrays
+    flat_images = [image.flatten() for image in images]
+
+    # Combine the flattened images into a single array
+    combined_images = np.vstack(flat_images)
+
+    # Mix the images using the random mixing matrix
+    mixed_images = np.dot(mixing_matrix, combined_images)
+
+    # Reshape the mixed images back to their original shape
+    mixed_images_list = [mixed_images[i].reshape(images[i].shape) for i in range(len(images))]
+
+    return mixed_images_list
--- test/unit/utils/test_image_ops.py
+++ test/unit/utils/test_image_ops.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-import numpy
+import numpy as np
 from PIL import Image
 
-from utils.image_ops import image_to_np_array, rgb_to_grayscale
+from utils.image_ops import image_to_np_array, mix_images, rgb_to_grayscale
 
 
 def test_rgb_to_grayscale(four_faces_path: Path):
@@ -15,5 +15,32 @@ def test_rgb_to_grayscale(four_faces_path: Path):
         grayscale_image_sample = four_faces_path / "grayscale" / rgb_image_sample.name
 
         expected_grayscale_image = image_to_np_array(grayscale_image_sample)
-        expected_grayscale_image = numpy.array(Image.open(grayscale_image_sample))
+        expected_grayscale_image = np.array(Image.open(grayscale_image_sample))
         assert (expected_grayscale_image == grayscale_image).all()
+
+
+def test_mix_images():
+    img1 = np.array([[4, 2], [6, 8]])
+    img2 = np.array([[2, 4], [8, 16]])
+
+    mixing_matrix = np.array([[0.5, 0.5], [0.5, 0.5]])
+    mixed_images = mix_images([img1, img2], mixing_matrix=mixing_matrix)
+    assert len(mixed_images) == 2
+
+    mixed_img1 = mixed_images[0]
+    expected_mixed_img1 = np.array([[3, 3], [7, 12]])
+    assert (mixed_img1 == expected_mixed_img1).all()
+    mixed_img2 = mixed_images[1]
+    expected_mixed_img2 = np.array([[3, 3], [7, 12]])
+    assert (mixed_img2 == expected_mixed_img2).all()
+
+    mixing_matrix = np.array([[0.25, 0.75], [0.75, 0.25]])
+    mixed_images = mix_images([img1, img2], mixing_matrix=mixing_matrix)
+
+    mixed_img1 = mixed_images[0]
+    expected_mixed_img1 = np.array([[2.5, 3.5], [7.5, 14]])
+    assert (mixed_img1 == expected_mixed_img1).all()
+
+    mixed_img2 = mixed_images[1]
+    expected_mixed_img2 = np.array([[3.5, 2.5], [6.5, 10]])
+    assert (mixed_img1 == expected_mixed_img1).all()
